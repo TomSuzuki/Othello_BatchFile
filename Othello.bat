3b110ed0ac04b90b9026ed93da0c9852e46879cd
Unknown <b1017055@fun.ac.jp>
2018-12-22 06:13:42 +0900

    バグあり
    コンピュータ側の処理が問題なのか置ける場所が問題なのか...

--- Othello.bat
+++ Othello.bat
@@ -121,9 +121,49 @@ rem ゲーム
 	)
 	
 	rem 置ける場所があるかチェック（両方なければ終了）
+	call :funcCHKcell
+	set /a num=0
+	for /l %%i in (0,1,7) do (
+		for /l %%j in (0,1,7) do (
+			set /a x=%%i
+			set /a y=%%j
+			call set /a num=!num!+%%CHKcell[!x!][!y!]%%
+		)
+	)
+	
+	rem 次のターンのプレイヤーが石を置けない
+	if num == 0 (
+		if !turn! == 1 (
+			set /a turn=2
+		) else (
+			set /a turn=1
+		)
+		call :funcCHKcell
+		set /a num=0
+		for /l %%i in (0,1,7) do (
+			for /l %%j in (0,1,7) do (
+				set /a x=%%i
+				set /a y=%%j
+				call set /a num=!num!+%%CHKcell[!x!][!y!]%%
+			)
+		)
+		if num == 0 ( goto :gameExit )
+	)
+	
+	goto :game
+	
+	pause
+	exit
+	
+:gameExit
 
-pause
-exit
+	call :draw
+	echo ------------------
+	echo ゲームが終了しました。
+	
+	pause
+	exit
+
 
 rem プレイヤーの処理
 :runPlayer
@@ -139,13 +179,87 @@ rem プレイヤーの処理
 		goto :runPlayer
 	)
 	
+	rem 置く
+	set /a putX=!px!
+	set /a putY=!py!
+	call :funcSETcell
+	
 	exit /b
 
 rem AIの処理
 :runAI
-
-	exit /b
 	
+	rem 多く返せるところに置く
+	set /a putX=-1
+	set /a putY=-1
+	set /a max=0
+	call :funcCHKcell
+	for /l %%i in (0,1,7) do (
+		for /l %%j in (0,1,7) do (
+			set /a x=%%i
+			set /a y=%%j
+			call set /a t=%%CHKcell[!x!][!y!]%%
+			if !t! gtr !max! (
+				set /a max=!t!
+				set /a putX=!x!
+				set /a putY=!y!
+			)
+		)
+	)
+		
+	rem 置く
+	call :funcSETcell
+	
+	exit /b
+
+rem 指定したセル（putX,putY）に置く（置けることが前提の関数）
+:funcSETcell
+	
+	set /a cell[!putX!][!putY!]=!turn!
+	
+	for /l %%f in (0,1,7) do (
+		call set /a wx=%%wayX[%%f]%%
+		call set /a wy=%%wayY[%%f]%%
+		set /a k=0
+		set /a add=0
+		set /a flg=1
+		for /l %%k in (1,1,7) do (
+			if !flg! == 1 (
+				set /a flg=0
+				set /a nx=!putX!+!wx!*%%k
+				set /a ny=!putY!+!wy!*%%k
+				if !nx! == -1 (
+					set /a add=0
+				) else if !nx! == 8 (
+					set /a add=0
+				) else if !ny! == -1 (
+					set /a add=0
+				) else if !ny! == 8 (
+					set /a add=0
+				) else (
+				 	call set /a t=%%cell[!nx!][!ny!]%%
+					if !t! == !turn! (
+						rem 自分の石なら処理を終了
+					) else if !t! == 0 (
+						rem 空白セルなら返せる数を0にし、処理を終了
+						set /a add=0
+					) else (
+						rem 相手の石のときなら返せる数を増やし処理を続行
+						set /a add=!add!+1
+						set /a flg=1
+					)
+				)
+			)
+		)
+		for /l %%k in (1,1,!add!) do (
+			set /a nx=!putX!+!wx!*%%k
+			set /a ny=!putY!+!wy!*%%k
+			set /a cell[!nx!][!ny!]=!turn!
+		)
+	)
+	
+	exit /b
+
 rem 置ける場所のチェックを行うための配列を生成する
 :funcCHKcell
 	rem CHKcellの初期化（そのセルにおいたときに返せる石の数が代入される）
@@ -185,10 +299,10 @@ rem 置ける場所のチェックを行うための配列を生成する
 						) else (
 						 	call set /a t=%%cell[!nx!][!ny!]%%
 							if !t! == !turn! (
-								rem 自分の石なら返せる数を0にし、処理を終了
-								set /a add=0
+								rem 自分の石なら処理を終了
 							) else if !t! == 0 (
-								rem 空白セルなら処理を終了
+								rem 空白セルなら返せる数を0にし、処理を終了
+								set /a add=0
 							) else (
 								rem 相手の石のときなら返せる数を増やし処理を続行
 								set /a add=!add!+1
@@ -200,7 +314,6 @@ rem 置ける場所のチェックを行うための配列を生成する
 				set /a cnt=!cnt!+!add!
 			)
 			set /a CHKcell[!cx!][!cy!]=!cnt!
-			call echo CHKcell[!cx!][!cy!]=%%CHKcell[!cx!][!cy!]%%
 		)
 	)
 
@@ -226,12 +339,12 @@ rem 盤面を描画する
 	for /l %%i in (0,1,7) do (
 		call set msg=%%textColumn[%%i]%%
 		for /l %%j in (0,1,7) do (
-			set /a x=%%i
-			set /a y=%%j
+			set /a x=%%j
+			set /a y=%%i
 			call set /a s=%%cell[!x!][!y!]%%
 			if !s! == 0 ( set msg=!msg!　)
-			if !s! == 1 ( set msg=!msg!●)
-			if !s! == 2 ( set msg=!msg!○)
+			if !s! == 1 ( set msg=!msg!!textDraw[1]!)
+			if !s! == 2 ( set msg=!msg!!textDraw[2]!)
 		)
 		echo !msg!
 	)
